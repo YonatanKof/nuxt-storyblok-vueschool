753a884d52346a18ca596d54ac461f7ab3cee3c8
Yonatan Kof <kof@yonatankof.com>
2023-02-09 19:58:15 +0200

    Add conetnt

--- app.vue
+++ app.vue
@@ -1,5 +1,10 @@
+<script setup>
+const story = await useStoryblok("home", { version: "draft" });
+</script>
 <template>
-  <div>
-    <NuxtWelcome />
-  </div>
+	<div>
+		<h1>Hi there</h1>
+		<h2>{{ story.content.body[0].title }}</h2>
+		<pre>{{ story }}</pre>
+	</div>
 </template>
