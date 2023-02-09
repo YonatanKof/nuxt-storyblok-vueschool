6866a03d921813bc6c3ea49498bcf6e0755a608a
Yonatan Kof <kof@yonatankof.com>
2023-02-09 19:57:52 +0200

    Add accessToken

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+	typescript: { shim: false },
+	modules: [
+		[
+			"@storyblok/nuxt",
+			{
+				accessToken: "ijc1kWxihfUKFEjmFmDeZAtt",
+			},
+		],
+	],
+});
