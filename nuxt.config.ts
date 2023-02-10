d79700d0961f24da2908c0696361067620acf65f
Yonatan Kof <kof@yonatankof.com>
2023-02-10 23:09:38 +0200

    Add SCSS

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	typescript: { shim: false },
+	css: ["@/assets/styles/style.scss"],
 	modules: [
 		[
 			"@storyblok/nuxt",
