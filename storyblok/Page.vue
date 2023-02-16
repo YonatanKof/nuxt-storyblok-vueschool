b0aa59c7eadbdf93a179202abd0094b7a4111cc1
Yonatan Kof <kof@yonatankof.com>
2023-02-16 19:16:15 +0200

    Consume SB content w/ Nuxt Page.vue component

---
+++ storyblok/Page.vue
@@ -0,0 +1,18 @@
+<!-- Define the prop as an object -->
+<script setup>
+defineProps({
+	blok: Object,
+});
+</script>
+<template>
+    <!-- StoryblokComponent is from Storyblok Nuxt module -->
+    <!-- we loop through the bloks and give it a key-->
+    <!-- this is to see the different bloks in this page-->
+	<div v-editable="blok">
+        <StoryblokComponent  
+            v-for="blok in blok.body" 
+            :key="blok._uid" 
+            :blok="blok"
+        ></StoryblokComponent>
+    </div>
+</template>
