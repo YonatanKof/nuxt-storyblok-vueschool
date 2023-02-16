635b5985ef95ad03420319aac96cabe98b784a6a
Yonatan Kof <kof@yonatankof.com>
2023-02-16 19:18:17 +0200

    Add the Hero component

---
+++ storyblok/Hero.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+	blok: Object,
+});
+</script>
+<template>
+	<div class="hero" v-editable="blok">
+		<div class="container">
+			<div class="col">
+				<img :src="blok.image.filename" :alt="blok.image.alt" />
+			</div>
+			<div class="col">
+				<h1>{{ blok.title }}</h1>
+				<h2>{{ blok.subtitle }}</h2>
+			</div>
+		</div>
+	</div>
+</template>
