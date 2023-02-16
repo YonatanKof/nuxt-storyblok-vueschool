a343335382c205ad05e49a5fe1a4b874b49c06de
Yonatan Kof <kof@yonatankof.com>
2023-02-16 19:18:28 +0200

    Move to file based routing - Create Pages folder - Create `index.vue` file - move code from `app.vue` to `index.vue`

--- app.vue
+++ app.vue
@@ -1,10 +1,3 @@
-<script setup>
-const story = await useStoryblok("home", { version: "draft" });
-</script>
 <template>
-	<div>
-		<h1>Hi there</h1>
-		<h2>{{ story.content.body[0].title }}</h2>
-		<pre>{{ story }}</pre>
-	</div>
+	<NuxtPage></NuxtPage>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<script setup>
+const story = await useStoryblok("home", { version: "draft" });
+</script>
+<template>
+	<!-- We use v-if since we have to await the result from the API -->
+	<StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+</template>
