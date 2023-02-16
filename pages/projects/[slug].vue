e08d40b230db06c1c3f4c19fba5926e758c9beb0
Yonatan Kof <kof@yonatankof.com>
2023-02-16 19:19:23 +0200

    Add the project dynamic file & its component

---
+++ pages/projects/[slug].vue
@@ -0,0 +1,9 @@
+<script setup>
+const route = useRoute();
+const story = await useStoryblok(`projects/${route.params.slug}`, { version: "draft" });
+</script>
+
+<template>
+	<!-- We use v-if since we have to await the result from the API -->
+	<StoryblokComponent v-if="story" :blok="story.content"></StoryblokComponent>
+</template>
---
+++ storyblok/Project.vue
@@ -0,0 +1,23 @@
+<script setup>
+defineProps({
+	blok: Object,
+});
+</script>
+<template>
+	<div class="container project-page">
+		<div class="project-title">
+			<h1>{{ blok?.title || "Add your title" }}</h1>
+			<NuxtLink :to="{ name: 'index' }">Go Back</NuxtLink>
+		</div>
+		<div class="project-description">
+			<img
+				:src="
+					blok.image?.filename ||
+					`https://a.storyblok.com/f/198303/1024x1024/1a434df4e4/yonatan_kof_two_mounting_and_sun_black_and_white_flat_design_si_c9597e13-43e9-42f7-9acb-9fb5e44c65dc.png/m/`
+				"
+				:alt="blok.image?.alt"
+			/>
+			<p>{{ blok?.description || "Add your description" }}</p>
+		</div>
+	</div>
+</template>
