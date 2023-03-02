b5f621bc8877fc7849af82c9b0d158685a13b894
Yonatan Kof <kof@yonatankof.com>
2023-03-02 19:18:27 +0200

    Better placeholders

--- storyblok/FeaturedProjects.vue
+++ storyblok/FeaturedProjects.vue
@@ -9,8 +9,14 @@ defineProps({
 		<div class="featured-projects">
 			<div class="project" v-for="project in blok.featuredProjects" :key="project._uid">
 				<nuxt-link :to="{ name: 'projects-slug', params: { slug: project.slug } }">
-					<h3>{{ project.content.title }}</h3>
-					<img :src="project.content.image?.filename" :alt="project.content.image?.alt || `No alt text yet`" />
+					<h3>{{ project.content.title || "Missing title" }}</h3>
+					<img
+						:src="
+							project.content.image?.filename ||
+							`https://a.storyblok.com/f/198303/1024x1024/1a434df4e4/yonatan_kof_two_mounting_and_sun_black_and_white_flat_design_si_c9597e13-43e9-42f7-9acb-9fb5e44c65dc.png/m/`
+						"
+						:alt="project.content.image?.alt || `No alt text yet`"
+					/>
 				</nuxt-link>
 			</div>
 		</div>
